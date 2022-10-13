f2a66d616298f71cf5744845ac666c5f0d14e1c5
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2022-10-13 10:36:54 -0400

    Doc string

--- exercice3.py
+++ exercice3.py
@@ -7,17 +7,31 @@ from math import pi
 
 
 class Cercle:
-
+    """
+    classe qui permet de creer un cercle avec le rayon
+    """
     def __init__(self, radius):
+        """
+        fonction appeler lors de la creation dun instance qui cree les attributs
+        :param radius: rayon du cercle
+        """
         self.radius = radius
         self.area = None
         self.circumference = None
 
     def calcul_aire(self):
+        """
+        fonction qui permet de calculer l'aire du cercle
+        :return: l'aire du cercle arrondis au centieme
+        """
         self.area = pi * self.radius ** 2
         return round(self.area, 2)
 
     def calcul_circonference(self):
+        """
+        fonction qui permet de calculer la circonference du cercle
+        :return: la circonference du cercle arrondis au centieme
+        """
         self.circumference = 2 * pi * self.radius
         return round(self.circumference, 2)
 
